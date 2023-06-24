a397b0972a8f970fc7b1307a14e8f063d189f99e
Mohamed <mohamed.bouhleel@gmail.com>
2023-06-24 18:10:07 +0200

    WIP

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,19 +1,64 @@
 
+
 import 'package:flutter/material.dart';
 
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(appBar: AppBar(
-    title: Text('toto'),
+void main() => runApp(const MaterialApp(
+  home: Home()
+));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+    title: const Text('TESTss'),
     centerTitle: true,
+    backgroundColor: const Color.fromARGB(255, 166, 22, 44),
   ),
-  body: Center(
-    child: 
-        Text('Hello'),
-            ),
-            floatingActionButton: FloatingActionButton(onPressed: () => null,
-              child: Text('ddd')
+  body:  Column(
+    children: [
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children:  [
+        const Text('dd'),const Text('data'),ElevatedButton(onPressed: () {}, child: const Text('momo'))
+      ],
+    ),    
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+crossAxisAlignment: CrossAxisAlignment.end,
+      children:  [
+        const Text('dd'),const Text('data'),ElevatedButton(onPressed: () {}, child: const Text('momo'))
+      ],
+    )
+    ]
+
+
+      ),
+      
+      
+    /*ElevatedButton(
+      onPressed: () => {
+        print('hello')
+      },
+          child: const Text('data')
+    ) ,*/
+        //Text('Hello'),
+    //Image(image: AssetImage('assets/test.jpeg')),
+           // Icon(Icons.chevron_left)
+            floatingActionButton: FloatingActionButton(onPressed: () => {},
+            backgroundColor: Colors.amber[200],
+              child: const Text('Click', 
+              style: TextStyle(
+                letterSpacing: 2,
+                color: Colors.red,
+                fontWeight: FontWeight.bold
+              )),
              ),
 
-  )
-));
+  );
+  }
+}
